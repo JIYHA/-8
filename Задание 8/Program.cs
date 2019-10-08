7dc61388c95455b1fba7ddacbfe7dbed717d2fbd
ya.zmeya.2001@gmail.com <ya.zmeya.2001@gmail.com>
2019-10-08 21:59:33 +0300

    Добавьте файлы проекта.

---
+++ Задание 8/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Задание_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите ваше число");
+            double x;
+            x = double.Parse(Console.ReadLine());
+            double y = 7 + x * (-1 + x * (2 + x * (-5 + 3 * x)));
+            Console.WriteLine("Ответ: " + y);
+        }
+    }
+}
